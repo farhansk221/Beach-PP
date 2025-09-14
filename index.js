b6f3d758bf2a4186113577ce8c6724b8dac5ba48
farhansk221 <2024.mohd.farhan.shaikh@ves.ac.in>
2025-09-14 10:48:39 +0530

    Changes index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const allowedOrigin = process.env.FRONTEND_ORIGIN || '*';
-app.use(cors({ origin: allowedOrigin }));
+app.use(cors());
 
 
 app.use('/api', signup);
